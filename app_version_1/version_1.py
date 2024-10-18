8f997dea05fa7af5b044771055acc304f5f977b3
Wuyang_Gao <wuyangg@umich.edu>
2024-10-17 22:08:07 -0400

    with university logo

--- app_version_1/version_1.py
+++ app_version_1/version_1.py
@@ -90,6 +90,14 @@ if selected_section == "Overview":
 #--------------------------------------------------------------------------------------------
 # following is for the university page
 elif selected_section == "University":
+
+    image_folder = 'datasets/university_logo/'
+    image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith(('png', 'jpg', 'jpeg'))]
+
+    # Display images in a grid
+    st.write("#### Top Universities")
+    cols = st.columns(8)  # Create 5 columns
+
     st.title("University Profiles in Biomedical Engineering")
     st.header("Universities faculty community and publications")
     st.markdown('''Biomedical Engineering blends engineering and biology to create cutting-edge medical technologies. 
